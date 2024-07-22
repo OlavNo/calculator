1ac9a363b25f7ba025607e75aa7a82638bd6ca47
Olav Nordvik <olavnordv@gmail.com>
2024-07-22 05:44:30 +0200

    added all functionality

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-let num1;
-let operator;
-let num2;
+let num1 = undefined;
+let operator = undefined;
+let num2 = undefined;
+let screenContent = "";
+let result = 0;
+let decimals = 5;
 
 const numbers = document.querySelectorAll(".numbers");
-const screen = document.querySelector("#screen");
+const writeText = document.querySelector("#writeText");
+const resultView = document.querySelector("#resultView");
+
 // we use the .forEach method to iterate through each button
 numbers.forEach((numberKey) => {
   // and for each one we add a 'click' listener
-  numberKey.addEventListener("click", () => {
-        screen.textContent += numberKey.id;
+    numberKey.addEventListener("click", () => {
+        if (numberKey.id === "." && containsDot()) {
+        }
+        else {
+            screenContent += numberKey.id;
+        }
+        writeText.textContent = screenContent;
   });
 });
 
@@ -18,21 +28,37 @@ const operators = document.querySelectorAll(".operator");
 operators.forEach((operatorKey) => {
     // and for each one we add a 'click' listener
 operatorKey.addEventListener("click", () => {
-        num1 = screen.textContent;
-        operator = operatorKey.id;
-        screen.textContent = "";
+    operator = operatorKey.id;
+    if (screenContent != "") {
+        num1 = parseFloat(screenContent);
+    }
+    else {
+        num1 = result;
+    }
+
+    resultView.textContent = Math.round(num1*Math.pow(10, decimals))/Math.pow(10, decimals) + " " + operator;
+    screenContent = "";
+    writeText.textContent = screenContent;
     });
 });
 
 const equals = document.querySelector(".equals");
 
 equals.addEventListener("click", () => {
-    num2 = screen.textContent;
-    let result = operate(Number(num1), Number(num2), operator);
-    console.log(result);
-    screen.textContent = result;
+    num2 = screenContent;
+    result = operate(parseFloat(num1), parseFloat(num2), operator);
+    clearContent();
+    resultView.textContent = Math.round(result*Math.pow(10, decimals))/Math.pow(10, decimals);
+    screenContent = "";
+    writeText.textContent = screenContent;
 });
 
+
+const clearCalc = document.querySelector(".AC");
+
+clearCalc.addEventListener("click", () => {
+    clearCalculator();
+});
 
 const operate = function(num1, num2, operator) {
     const operatorRelation = {
@@ -67,10 +93,30 @@ const power = function(num, pow) {
   return Math.pow(num, pow);
 };
 
-const factorial = function(num) {
-  var fac = 1;
-	for (var i = 1; i <= num; i++) {
-    fac *= i;
-  }
-  return fac;
-};
+
+const clearCalculator = function() {
+    num1 = undefined;
+    operator = undefined;
+    num2 = undefined;
+    screenContent = "";
+    result = 0;
+    writeText.textContent = "";
+    resultView.textContent = "";
+
+}
+
+const containsDot = function() {
+    var dot = false;
+    for (var i = 0; i < screenContent.length; i++) {
+        if (screenContent.charAt(i) === '.') {
+            dot = true;
+            break;
+        }
+    }
+    return dot;
+}
+
+let clearContent = function() {
+    num2 = undefined;
+    operator = undefined;
+}
